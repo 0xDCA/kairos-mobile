e346f1b2078656b5e3709ad2a4ef1ccd73f7600d
Daniel Castro <dancas2@gmail.com>
2016-01-13 18:53:21 -0500

    First commit

---
+++ www/js/app.js
@@ -0,0 +1,50 @@
+angular.module('kairos', ['ionic', 'kairos.controllers'])
+
+.run(function($ionicPlatform, $window) {
+  'use strict';
+
+  $ionicPlatform.ready(function() {
+    // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
+    // for form inputs)
+    if ($window.cordova && $window.cordova.plugins.Keyboard) {
+      $window.cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+      $window.cordova.plugins.Keyboard.disableScroll(true);
+    }
+
+    if ($window.StatusBar) {
+      // org.apache.cordova.statusbar required
+      $window.StatusBar.styleDefault();
+    }
+  });
+})
+
+.config(function($stateProvider, $urlRouterProvider) {
+  'use strict';
+
+  $stateProvider
+  .state('app', {
+    url: '/app',
+    abstract: true,
+    templateUrl: 'templates/menu.html',
+    controller: 'AppController'
+  })
+  .state('app.start', {
+    url: '/start',
+    views: {
+      'menuContent': {
+        templateUrl: 'templates/start.html'
+      }
+    }
+  })
+  .state('app.savedSchedules', {
+    url: '/saved-schedules',
+    views: {
+      'menuContent': {
+        templateUrl: 'templates/saved-schedules.html'
+      }
+    }
+  });
+
+  // if none of the above states are matched, use this as the fallback
+  $urlRouterProvider.otherwise('/app/start');
+});
---
+++ www/js/controllers.js
@@ -0,0 +1,9 @@
+angular.module('kairos.controllers', [])
+
+.controller('AppController', function($scope, $ionicModal, $timeout) {
+  'use strict';
+})
+
+.controller('PlaylistsCtrl', function($scope) {
+  'use strict';
+});
